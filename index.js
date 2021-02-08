a1f099effedb3d19df3af54277d2b49cd9e986af
sakatanitouga <sakatani.touga@gmail.com>
2021-02-08 12:29:23 +0900

    Edit_js

--- index.js
+++ index.js
@@ -46,9 +46,7 @@
 	  xhr.onreadystatechange = function () {
 	  	if (xhr.readyState == 4 && xhr.status == 200) {
 	  		var response = JSON.parse(this.responseText);
-		    if (response.result != 0) {
 		    	console.log(response);
-		    }
 	  	}
 	  }
 	  xhr.send(null);
